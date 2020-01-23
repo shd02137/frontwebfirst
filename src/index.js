266ed75c1a75646242daf54b8ccf688e5d746d7b
anaconda <shd02137@naver.com>
2020-01-23 16:54:06 +0900

    new website

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const title = 'is it work?';
+ 
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: [
+    './src/index.js',
+    './src/timer.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
