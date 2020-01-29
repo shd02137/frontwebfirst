28a9fe9d97d4267c2aa46a03aa91de7f90bebf11
unknown <shd02137@naver.com>
2020-01-29 13:03:18 +0900

    add feature

--- src/mainContent.js
+++ src/mainContent.js
@@ -10,22 +10,17 @@ const title = 'now main start';
 ReactDOM.render(
   <div>{title}
   	<div id = "skip-to-content">
-  		<div class = "container ">
-  			<div class ="row">
-  				<div class = "column-6 tablet-hide">
-  					<h4>hi,world</h4>
-            <div id="subnav">
-            
-
-            </div>
-
-  				</div>
-  				<div class="column-17 tablet-column-12 pre-1 content-face">
-  					<h4>now we can work</h4>
-  				</div>
-
-
+  		<div class ="row">
+  			<div class = "column-6 tablet-hide">
+  				<h4>hi,world</h4>
+          <div id="Subnav"></div>
   			</div>
+  			<div class="column-17 tablet-column-12 pre-1 content-face">
+  				<h4>now we can work</h4>
+          <iframe src="./right.html" width = "100%" height = "100%"frameborder="0" framespacing="0" marginheight="0" marginwidth="0" vspace="0"></iframe>
+          
+  			</div>
+  		
   		</div>
   	</div>
 
--- src/subna.js
+++ src/subna.js
@@ -12,7 +12,7 @@ ReactDOM.render(
   document.getElementById('subnav')
 );
 */
-
+/*작동안됨
 const MenuItem = ({active, children, to}) => (
     <div className="menu-item">
             {children}
@@ -33,3 +33,46 @@ const Header = () => {
         </div>
     );
 };
+*/
+class Subnav extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>subnavtest</h2>
+                    <nav class="navmenu navbar-default" role="navigation">
+                        <a class="navbar-brand" href="#">Brand</a>
+
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Link</a></li>
+                            <li><a href="#">Link</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                                <ul class="dropdown-menu navbar-nav" role="menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                <button type="button" class="btn">Basic</button>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <div>
+        <Subnav />
+        
+    </div>,
+    document.getElementById('Subnav')
+);
+
